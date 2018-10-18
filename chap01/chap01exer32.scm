6402e7cb98a6fc991828d86b0c1f89cc5c950e3b
David Pritchard <dpritch@live.unc.edu>
2018-10-17 23:23:48 -0400

    add exercise 1.32

---
+++ chap01/chap01exer32.scm
@@ -0,0 +1,62 @@
+;; *Exercise 1.32:*
+;;
+;;   a. Show that `sum` and `product` (*Note Exercise 1-31) are both special
+;;   cases of a still more general notion called `accumulate` that combines a
+;;   collection of terms, using some general accumulation function:
+;;
+;;           (accumulate combiner null-value term a next b)
+;;
+;;   `Accumulate` takes as arguments the same term and range specifications as
+;;   `sum` and `product`, together with a `combiner` procedure (of two
+;;   arguments) that specifies how the current term is to be combined with the
+;;   accumulation of the preceding terms and a `null-value` that specifies what
+;;   base value to use when the terms run out.  Write `accumulate` and show how
+;;   `sum` and `product` can both be defined as simple calls to `accumulate`.
+;;
+;;   b. If your `accumulate` procedure generates a recursive process, write one
+;;   that generates an iterative process.  If it generates an iterative process,
+;;   write one that generates a recursive process.
+;;
+;; -----------------------------------------------------------------------------
+
+
+;; part a (linear version) -----------------------------------------------------
+
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+		(accumulate combiner null-value term (next a) next b))))
+
+
+
+
+;; part b (iterative version) --------------------------------------------------
+
+(define (accumulate-iter combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+       result
+       (iter (next a) (combiner result (term a)))))
+  (iter a null-value))
+
+
+
+
+;; test cases ------------------------------------------------------------------
+
+(define (identity x) x)
+(define (square x) (* x x))
+
+
+;; sum_{i = 1}^{10} i = 55
+(accumulate + 0 identity 1 1+ 10)
+(accumulate-iter + 0 identity 1 1+ 10)
+
+;; sum_{i = 1}^{4} i^2 = 30
+(accumulate + 0 square 1 1+ 4)
+(accumulate-iter + 0 square 1 1+ 4)
+
+;; prod_{i = 1}^{5} i = 120
+(accumulate * 1 identity 1 1+ 5)
+(accumulate-iter * 1 identity 1 1+ 5)
